cfcdad6bfdf4fffbb5d3083a445ea34089575365
naga-georgian <nagavenkatasarat.rimmalapudi@mygeorgian.ca>
2016-12-16 22:44:19 -0500

    Other Utilitiy functions

--- COMP1004-F2016-SlotMachine/SlotMachineForm.cs
+++ COMP1004-F2016-SlotMachine/SlotMachineForm.cs
@@ -90,6 +90,43 @@ namespace COMP1004_F2016_SlotMachine
             lbl_Credits.Text = playerMoney.ToString();
             lbl_Bet.Text = playerBet.ToString();
         }
+        /* Check to see if the player won the jackpot */
+        private void checkJackPot()
+        {
+            /* compare two random values */
+            var jackPotTry = this.random.Next(51) + 1;
+            var jackPotWin = this.random.Next(51) + 1;
+            if (jackPotTry == jackPotWin)
+            {
+                lbl_Paid.Text = "JACKPOT!!";
+                MessageBox.Show("You Won the $" + jackpot + " Jackpot!!", "Jackpot!!");
+                playerMoney += jackpot;
+                jackpot = 1000;
+            }
+        }
+
+        /* Utility function to show a win message and increase player money */
+        private void showWinMessage()
+        {
+            playerMoney += winnings;
+            lbl_Paid.Text = "Won: $" + winnings;
+            resetFruitTally();
+            checkJackPot();
+        }
+
+        /* Utility function to show a loss message and reduce player money */
+        private void showLossMessage()
+        {
+            lbl_Paid.Text = "Lost!";
+            resetFruitTally();
+        }
+
+        /* Utility function to check if a value falls within a range of bounds */
+        private bool checkRange(int value, int lowerBounds, int upperBounds)
+        {
+            return (value >= lowerBounds && value <= upperBounds) ? true : false;
+        }
+
 
     }
 }
