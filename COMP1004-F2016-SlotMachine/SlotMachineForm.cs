466c5223ee5d8743053bea7857ce8694beb6ea95
naga-georgian <nagavenkatasarat.rimmalapudi@mygeorgian.ca>
2016-12-16 22:40:51 -0500

    Utility function to show Player Stats and variables declaration

--- COMP1004-F2016-SlotMachine/SlotMachineForm.cs
+++ COMP1004-F2016-SlotMachine/SlotMachineForm.cs
@@ -20,5 +20,44 @@ namespace COMP1004_F2016_SlotMachine
             InitializeComponent();
         }
 
+        private int playerMoney = 500;
+        private int winnings = 0;
+        private int jackpot = 5000;
+        private float turn = 0.0f;
+        private int playerBet = 0;
+        private float winNumber = 0.0f;
+        private float lossNumber = 0.0f;
+        private string[] spinResult;
+        private string fruits = "";
+        private float winRatio = 0.0f;
+        private float lossRatio = 0.0f;
+        private int grapes = 0;
+        private int bananas = 0;
+        private int oranges = 0;
+        private int cherries = 0;
+        private int bars = 0;
+        private int bells = 0;
+        private int sevens = 0;
+        private int blanks = 0;
+
+        private Random random = new Random();
+
+
+
+        /* Utility function to show Player Stats */
+        private void showPlayerStats()
+        {
+            winRatio = winNumber / turn;
+            lossRatio = lossNumber / turn;
+            string stats = "";
+            stats += ("Jackpot: " + jackpot + "\n");
+            stats += ("Player Money: " + playerMoney + "\n");
+            stats += ("Turn: " + turn + "\n");
+            stats += ("Wins: " + winNumber + "\n");
+            stats += ("Losses: " + lossNumber + "\n");
+            stats += ("Win Ratio: " + (winRatio * 100) + "%\n");
+            stats += ("Loss Ratio: " + (lossRatio * 100) + "%\n");
+            MessageBox.Show(stats, "Player Stats");
+        }
     }
 }
